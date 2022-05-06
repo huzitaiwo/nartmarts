b0a9ee61552e726b4a363e0610aa63cc14138752
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-05-06 10:33:32 +0100

    created header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,17 @@ import React from 'react'
 export default function Header() {
   return (
     <div className='header'>
-        
+     <ul>
+        <li>
+          <span>Hyderabad, India</span>
+        </li>
+        <li>
+          <span>+91 9969922348</span>
+        </li>
+        <li>
+          <span>Import.Export@mail.com</span>
+        </li>
+     </ul>
     </div>
   )
 }
