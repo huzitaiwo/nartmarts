2f239d7bfb0c2fa473598cfc10df5f44cc42aadb
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-05-07 10:43:51 +0100

    created Navbar component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
-import React from 'react'
 import Header from '../components/Header'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <div className='home'>
       <Header />
+      <Navbar />
     </div>
   )
 }
