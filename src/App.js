bddb2197d0313ed83fba6a8374a77e08cbee4b51
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-05-08 10:46:05 +0100

    setup homepage route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
