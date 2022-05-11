740ffb9d81cd65ad53c15f73d6cb2c9279423547
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-05-11 10:59:16 +0100

    addeed icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,22 @@
-import React from 'react'
+//icons
+import callIcon from '../assets/Call.svg'
 
 export default function Header() {
   return (
     <div className='header'>
-      <ul className='address'>
-        <li>
-          <span>Hyderabad, India</span>
-        </li>
-        <li>
-          <span>+91 9969922348</span>
-        </li>
-        <li>
-          <span>Import.Export@mail.com</span>
-        </li>
-      </ul>
+        <div className="container">
+            <ul className='address'>
+                <li>
+                <span>Hyderabad, India</span>
+                </li>
+                <li>
+                <span>+91 9969922348</span>
+                </li>
+                <li>
+                <span>Import.Export@mail.com</span>
+                </li>
+            </ul>
+        </div>
     </div>
   )
 }
