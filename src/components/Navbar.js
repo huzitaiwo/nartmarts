0dea0d7abba6bade5882d155c269a511bc3a223a
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-05-13 11:08:47 +0100

    styling navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,17 @@ import logo from '../assets/logo.svg'
 export default function Navbar() {
   return (
     <nav className='navbar'>
-      <div className="logo">
-        Import <img src={logo} alt="" /> Export
-      </div>
-      <ul className="nav-links">
-        <li><Link>Home</Link></li>
-        <li><Link>About</Link></li>
-        <li><Link>Contact us</Link></li>
-        <li><Link>Shop</Link></li>
-      </ul>
+        <div className="container">
+            <div className="logo">
+                Import <img src={logo} alt="" /> Export
+            </div>
+            <ul className="nav-links">
+                <li><Link>Home</Link></li>
+                <li><Link>About</Link></li>
+                <li><Link>Contact us</Link></li>
+                <li><Link>Shop</Link></li>
+            </ul>
+        </div>
     </nav>
   )
 }
