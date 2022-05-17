ef090aa1c5a2dbc6f787865d65e36c5f839dd4f2
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-05-17 11:22:41 +0100

    created Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,11 @@ import banner from '../assets/hero_image.png'
 
 export default function Banner() {
   return (
-    <div>
-        <img src={banner} alt="banner" />
+    <div className='banner'>
+      <img src={banner} alt="banner" />
+      <h3>Making Transport</h3>
+      <h1>FAST & SAFE</h1>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean nascetur ridiculus mus dolor sit amet, consectetu.</p>
     </div>
   )
 }
