2dd54afe0d8780c6403825c45d72d465af6f0c8e
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-05-18 11:30:55 +0100

    created Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,9 +4,11 @@ export default function Banner() {
   return (
     <div className='banner'>
       <img src={banner} alt="banner" />
-      <h3>Making Transport</h3>
-      <h1>FAST & SAFE</h1>
-      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean nascetur ridiculus mus dolor sit amet, consectetu.</p>
+      <div className="container">
+        <h3>Making Transport</h3>
+        <h1>FAST & SAFE</h1>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean nascetur ridiculus mus dolor sit amet, consectetu.</p>
+      </div>
     </div>
   )
 }
